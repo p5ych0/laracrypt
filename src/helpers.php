360b3be5c3a352ee640dbd6f6f629fc1800434db
Kostiantyn Karnasevych <constantine.karnacevych@gmail.com>
2019-05-07 17:43:09 +0300

    conditional function definition in helper

--- src/helpers.php
+++ src/helpers.php
@@ -16,63 +16,71 @@ if (!function_exists("safeBase64")) {
     }
 }
 
-/**
- * Format a 32 chars string into UUID format
- *
- * @param  string                    $str
- * @throws \InvalidArgumentException
- * @return string
- */
-function as_uuid(string $str): string
-{
-    if (!preg_match("#^[a-f\d]{32}#", $str)) {
-        throw new InvalidArgumentException("Only hexadecimals in lower case can be accepted");
-    }
-
-    return substr($str, 0, 8) . "-" . substr($str, 8, 4) . "-" . substr($str, 12, 4)  . "-" . substr($str, 16, 4)  . "-" . substr($str, 20, 12);
-}
-
-/**
- * Check whether match UUID format
- *
- * @param  string $str Lowercased
- * @return bool
- */
-function is_uuid(string $str): bool
-{
-    return preg_match("#^[a-f\d]{8}-(?:[a-f\d]{4}-){3}[a-f\d]{12}$#", $str) > 0;
-}
-
-/**
- * Whether number is prime
- *
- * @param  int  $number
- * @return bool
- */
-function is_prime(int $number): bool
-{
-    return in_array($number, [2, 3, 5], true) || ($number > 1 && $number % 2 >= 1 && $number % 3 >= 1 && $number % 5 >= 1);
-}
-
-/**
- * Find next prime number under 100000
- *
- * @param  int                        $number
- * @throws \]InvalidArgumentException
- * @throws \DomainException
- * @return int
- */
-function next_prime(int $number): int
-{
-    if ($number < 0) {
-        throw new InvalidArgumentException("Number must be positive");
-    }
-
-    while (is_prime($number) === false) {
-        if ($number++ > 100000) {
-            throw new DomainException("Only prime numbers in range of 2..100000 are allowed");
+if (!function_exists("as_uuid")) {
+    /**
+     * Format a 32 chars string into UUID format
+     *
+     * @param  string                    $str
+     * @throws \InvalidArgumentException
+     * @return string
+     */
+    function as_uuid(string $str): string
+    {
+        if (!preg_match("#^[a-f\d]{32}#", $str)) {
+            throw new InvalidArgumentException("Only hexadecimals in lower case can be accepted");
         }
-    }
 
-    return $number;
+        return substr($str, 0, 8) . "-" . substr($str, 8, 4) . "-" . substr($str, 12, 4)  . "-" . substr($str, 16, 4)  . "-" . substr($str, 20, 12);
+    }
+}
+
+if (!function_exists("is_uuid")) {
+    /**
+     * Check whether match UUID format
+     *
+     * @param  string $str Lowercased
+     * @return bool
+     */
+    function is_uuid(string $str): bool
+    {
+        return preg_match("#^[a-f\d]{8}-(?:[a-f\d]{4}-){3}[a-f\d]{12}$#", $str) > 0;
+    }
+}
+
+if (!function_exists("is_prime")) {
+    /**
+     * Whether number is prime
+     *
+     * @param  int  $number
+     * @return bool
+     */
+    function is_prime(int $number): bool
+    {
+        return in_array($number, [2, 3, 5], true) || ($number > 1 && $number % 2 >= 1 && $number % 3 >= 1 && $number % 5 >= 1);
+    }
+}
+
+if (!function_exists("next_prime")) {
+    /**
+     * Find next prime number under 100000
+     *
+     * @param  int                        $number
+     * @throws \]InvalidArgumentException
+     * @throws \DomainException
+     * @return int
+     */
+    function next_prime(int $number): int
+    {
+        if ($number < 0) {
+            throw new InvalidArgumentException("Number must be positive");
+        }
+
+        while (is_prime($number) === false) {
+            if ($number++ > 100000) {
+                throw new DomainException("Only prime numbers in range of 2..100000 are allowed");
+            }
+        }
+
+        return $number;
+    }
 }
